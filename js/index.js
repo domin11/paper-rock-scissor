cf1effdbc2700d0f4c7af2fbfa63f45e083f32ac
Dominik Soboń <dominik.sobon@gmail.com>
2018-08-02 12:01:18 +0200

    Add tables in modals

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ var params = {
   computerScore : 0,
   maxRounds : 0,
   completeRounds : 0,
+  progress: [],
 };
 
 //funkcja blokująca przyciski po ukończonej grze
@@ -42,6 +43,7 @@ var scoreDestination = function(){
   document.getElementById('completeRounds').innerHTML = params.completeRounds;
 };
 
+//funkcja przypisująca ruchy gracza
 var playerButtons = document.querySelectorAll('.player-move');
 var playerButtonsLength = playerButtons.length;
 for (var i = 0; i < playerButtonsLength; i++) {
@@ -50,12 +52,20 @@ for (var i = 0; i < playerButtonsLength; i++) {
     });
 };
 
-buttonNewGame.addEventListener('click', function() {
-  params.maxRounds = window.prompt('Podaj liczbę rund do rozegrania');
-
+//funkcja resetująca wyniki
+var reset = function(){
   params.playerScore = 0;
   params.computerScore = 0;
   params.completeRounds = 0;
+  params.progress = [];
+}
+
+//przycisk nowej gry
+buttonNewGame.addEventListener('click', function() {
+  reset();
+
+  params.maxRounds = window.prompt('Podaj liczbę rund do rozegrania');
+
   document.getElementById('maxRounds').innerHTML = params.maxRounds;
   scoreDestination();
 
@@ -69,6 +79,41 @@ buttonNewGame.addEventListener('click', function() {
   }
 });
 
+//Modal
+var showModal = function(selector) {
+  document.querySelector('#'+selector).classList.add('show');
+    buildTable(selector);
+};
+
+var hideModal = function(selector) {
+  document.querySelector(selector).classList.remove('show');
+  blockButtons ();
+  buttonNewGame.classList.remove('hidden');
+};
+
+var closeButtons = document.querySelectorAll('.modal');
+for (var i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener('click', function(event) {
+        hideModal('#' + event.currentTarget.className.split(' ')[1]);
+    });
+}
+
+//Tabela
+var buildTable = function(selector) {
+    var tbody = document.querySelector('#tbody-'+selector);
+    params.progress.forEach(function(progressResult) {
+        var row = document.createElement('tr');
+        tbody.appendChild(row);
+        for (var key in progressResult) {
+            buildTableTd(progressResult[key], row);
+        }
+    })
+};
+var buildTableTd = function(value, row) {
+    var td = document.createElement('td');
+    td.innerHTML = value;
+    row.appendChild(td);
+};
 
 //funkcja przypisująca ruchy komputera
 function getComputerMove() {
@@ -92,23 +137,6 @@ var playerMove = function(playerMove) {
   displayResults(winner, playerMove, computerMove);
 };
 
-//Modal
-var showModal = function(selector) {
-    document.querySelector('#'+selector).classList.add('show');
-    buttonNewGame.classList.remove('hidden');
-};
-
-var hideModal = function(selector) {
-    document.querySelector(selector).classList.remove('show');
-};
-
-var closeButtons = document.querySelectorAll('.modal');
-for (var i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].addEventListener('click', function(event) {
-        hideModal('#' + event.currentTarget.className.split(' ')[1]);
-    });
-}
-
 //funkcja odpowiedzialna za wyswietlenia wyników
 var displayResults = function(winner, playerMove, computerMove) {
   if (winner === 'none') {
@@ -126,18 +154,23 @@ var displayResults = function(winner, playerMove, computerMove) {
   params.completeRounds++;
   }
   scoreDestination();
+  params.progress.push({
+    gameRounds: params.completeRounds,
+    gamePlayerMove: playerMove,
+    gameComputerMove: computerMove,
+    finalResult: params.playerScore + ' - ' + params.computerScore
+});
 
 //Wynik końcowy
-  if (params.maxRounds == params.completeRounds && params.playerScore > params.computerScore) {
+  if (params.maxRounds == params.completeRounds){
+    if (params.playerScore > params.computerScore) {
     showModal('win');
-    blockButtons ();
     }
-  else if (params.maxRounds == params.completeRounds && params.playerScore < params.computerScore) {
+    else if (params.playerScore < params.computerScore) {
     showModal('lost');
-    blockButtons ();
     }
-  else if (params.maxRounds == params.completeRounds && params.playerScore == params.computerScore) {
+    else if (params.playerScore == params.computerScore) {
     showModal('draw');
-    blockButtons ();
     }
-};  
+   }
+};
